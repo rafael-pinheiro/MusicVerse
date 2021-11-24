c6b1c91d3028e0386e308ecf82078375ed4e54d9
Rafael Pinheiro <moriarty.pinheiro@gmail.com>
2021-11-24 22:00:14 +0100

    updating config parsing

---
+++ config.ts
@@ -0,0 +1,9 @@
+const config = {
+  "spotify": {
+    "client_id": process.env.spotify_client_id,
+    "client_secret": process.env.spotify_client_secret,
+    "redirect_uri": process.env.spotify_redirect_uri
+  }
+};
+
+export default config;
