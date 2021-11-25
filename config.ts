ebbd0eff540e7e203ebafb7a717ad1ceaed855ef
Rafael Pinheiro <moriarty.pinheiro@gmail.com>
2021-11-25 22:37:05 +0100

    Ensuring env variables are set

--- config.ts
+++ config.ts
@@ -1,12 +1,20 @@
+const getEnvVar = (name: string): string => {
+  if (!process.env[name]) {
+    throw new Error(`Missing environment variable ${name}. Either add a ".env" file or set it on deployment`);
+  }
+
+  return process.env[name] as string;
+}
+
 const config = {
-  "spotify": {
-    "client_id": process.env.spotify_client_id,
-    "client_secret": process.env.spotify_client_secret,
-    "redirect_uri": process.env.spotify_redirect_uri
+  spotify: {
+    client_id: getEnvVar('spotify_client_id'),
+    client_secret: getEnvVar('spotify_client_secret'),
+    redirect_uri: getEnvVar('spotify_redirect_uri'),
   },
   session: {
-    secret: process.env.session_secret
-  }
+    secret: getEnvVar('session_secret'),
+  },
 };
 
 export default config;
